16fd2173dfceae16811620ea1b747a050856fa60
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-04-26 21:55:38 +0200

    add AlgodService#getBlockNumberFlux

--- src/main/kotlin/se/newton/stockpriceclient/services/AlgodService.kt
+++ src/main/kotlin/se/newton/stockpriceclient/services/AlgodService.kt
@@ -3,11 +3,14 @@ package se.newton.stockpriceclient.services
 import com.algorand.algosdk.v2.client.model.Account
 import com.algorand.algosdk.v2.client.model.BlockResponse
 import com.algorand.algosdk.v2.client.model.NodeStatusResponse
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface AlgodService {
 	fun getAccountInformation(wallet: String): Mono<Account>
 	fun getStatus(): Mono<NodeStatusResponse>
 	fun getLatestBlock(): Mono<BlockResponse>
+	fun getLatestBlockNumber(): Mono<Long>
 	fun getNextBlock(): Mono<BlockResponse>
+	fun getBlockNumberFlux(): Flux<Long>
 }
