52fa28f018fc3ad5576da4d652b9f88964ba0de5
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-04-26 21:16:39 +0200

    make AlgodTestController => AlgodController

--- src/main/kotlin/se/newton/stockpriceclient/controller/rest/AlgodController.kt
+++ src/main/kotlin/se/newton/stockpriceclient/controller/rest/AlgodController.kt
@@ -1,30 +1,31 @@
 package se.newton.stockpriceclient.controller.rest
 
-import com.algorand.algosdk.crypto.Address
-import com.algorand.algosdk.v2.client.common.AlgodClient
-import com.algorand.algosdk.v2.client.model.Account
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import se.newton.stockpriceclient.services.AlgodServiceImpl
 
 /**
  * Normal REST controller for testing out the AlgodClient.
  * Useful mostly as reference.
  */
 @RestController
-@RequestMapping("/algod-test")
-class AlgodTestController(
-	val algod: AlgodClient
+@RequestMapping("/algo")
+class AlgodController(
+	val algod: AlgodServiceImpl
 ) {
 	@GetMapping("/wallet/{address}")
 	fun getWalletData(
 		@PathVariable("address") walletAddress: String
-	) : Account {
-		val response = algod.AccountInformation(Address(walletAddress)).execute()
-		if (!response.isSuccessful) {
-			throw Exception("Lookup failed!")
-		}
-		return response.body() as Account
-	}
+	) = algod.getAccountInformation(walletAddress)
+
+	@GetMapping("/status")
+	fun getStatus() = algod.getStatus()
+
+	@GetMapping("/block/latest")
+	fun getLastBlock() = algod.getLatestBlock()
+
+	@GetMapping("/block/next")
+	fun getNextBlock() = algod.getNextBlock()
 }
