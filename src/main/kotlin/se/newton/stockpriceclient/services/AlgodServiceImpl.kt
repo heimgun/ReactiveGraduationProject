f1974986149b74a6affacad3fd6e740ebfd4295a
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-06-06 15:57:26 +0200

    add lastResponseFlux print for demo purposes

--- src/main/kotlin/se/newton/stockpriceclient/services/AlgodServiceImpl.kt
+++ src/main/kotlin/se/newton/stockpriceclient/services/AlgodServiceImpl.kt
@@ -25,6 +25,7 @@ class AlgodServiceImpl(
 				val startRound = nodeStatusResponse.lastRound - 1
 				val firstBlock = algod.WaitForBlock(startRound).execute().extractOrFail()
 				return@flatMapMany generateSequence(firstBlock) {
+					println("lastResponseFlux processed: ${it.lastRound}")
 					algod.WaitForBlock(it.lastRound).execute().extractOrFail()
 				}.toFlux()
 			}.cache(1)
